8e20a21d1cd9c9f3db455a49e618f86ceb41eba3
islong <isaac.longkutoy@gmail.com>
2020-01-04 09:45:53 -0500

    Initialize Spring backend

---
+++ server/src/test/java/com/concourse/ConcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
