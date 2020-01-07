cb77a6c41dd069634a95926a02edef2cc14bf962
islong <isaac.longkutoy@gmail.com>
2020-01-06 21:27:17 -0500

    Added docs/ for GH Pages

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5000' //BACKEND API URL HERE
+  apiUrl: 'http://concourse-env.m82hjppbnt.us-east-2.elasticbeanstalk.com' //BACKEND API URL HERE
 };
